449c14c1ae7ec59cb87ee082fbe239345a7e0fee
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-04-09 12:20:36 -0500

    correccion: poner el environment.ts real

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 // src/environments/environment.ts
 
 // Usar esta para PRUEBAS LOCALES
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api'
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api'
+// };
 
 
 // Usar esta en PRODUCCION (Render)
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://rydex-api.onrender.com/api' 
-// };
+export const environment = {
+  production: false,
+  apiUrl: 'https://rydex-api.onrender.com/api' 
+};
